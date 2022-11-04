1dbcf4cc2b7b12fb864615a9d3f0cbaa356da456
Arpan KC <nipeshkc7@gmail.com>
2022-11-05 10:47:45 +1100

    initialize next app

---
+++ app/blog/layout.js
@@ -0,0 +1,4 @@
+// app/blog/layout.js
+export default function BlogLayout({ children }) {
+    return <section>{children}</section>;
+  }
---
+++ app/page.js
@@ -0,0 +1,5 @@
+// app/page.js
+// This file maps to the index route (/)
+export default function Page() {
+    return <div>Hello there !!!!</div>
+}
