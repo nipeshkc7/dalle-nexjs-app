19259c408736fa7bce15554da50c0860878a82f4
Arpan KC <nipeshkc7@gmail.com>
2022-11-05 11:04:42 +1100

    add generateAI function

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
 // app/page.js
 // This file maps to the index route (/)
-export default function Page() {
+
+async function generateAIImage(prompt){
+    const response = await openai.createImage({
+        prompt,
+        n: 1,
+        size: "1024x1024",
+      });
+    
+    if(!response.data) throw new Error('Unable to get image');
+    return response.data.data[0].url;
+}
+
+export default function SearchPage() {
     return <div>Hello there !!!!</div>
 }
